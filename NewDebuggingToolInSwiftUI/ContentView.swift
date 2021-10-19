0bab562dbd24139f0258a0ebc0486c043e0a06ae
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-10-19 20:10:59 +0300

    init

--- NewDebuggingToolInSwiftUI/ContentView.swift
+++ NewDebuggingToolInSwiftUI/ContentView.swift
@@ -7,13 +7,35 @@
 
 import SwiftUI
 
-struct ContentView: View {
+@available(iOS 15.0, *)
+struct SomeNewView: View {
+    @Binding var count: String
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        // debugger
+        let _ = Self._printChanges()
+        
+        Text("Count: \(count)")
             .padding()
     }
 }
 
+@available(iOS 15.0, *)
+struct ContentView: View {
+    static let numbers = ["One", "Two", "Three", "Four", "Five", "Six", "Seven"]
+    
+    @State var count: String = numbers.randomElement()!
+    
+    var body: some View {
+        SomeNewView(count: $count)
+            .onReceive(Timer.publish(every: 1, on: .main, in: .common).autoconnect()) { _ in
+                count = ContentView.numbers.randomElement()!
+            }
+    }
+}
+
+@available(iOS 15.0, *)
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
