5c1707d29296babee67647d6267b0798dbdf67f1
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-10-19 19:56:03 +0300

    Initial Commit

---
+++ NewDebuggingToolInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewDebuggingToolInSwiftUI
+//
+//  Created by Ramill Ibragimov on 19.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewDebuggingToolInSwiftUI/NewDebuggingToolInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewDebuggingToolInSwiftUIApp.swift
+//  NewDebuggingToolInSwiftUI
+//
+//  Created by Ramill Ibragimov on 19.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewDebuggingToolInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
